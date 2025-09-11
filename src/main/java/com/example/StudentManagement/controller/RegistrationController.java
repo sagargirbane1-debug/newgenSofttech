c47c495c5d5ca1da6f8ea103b64da037b085f614
Sagar Girbane <your_email@example.com>
2025-09-11 23:51:00 +0530

    added registration entity and registration controller class

---
+++ src/main/java/com/example/StudentManagement/controller/RegistrationController.java
@@ -0,0 +1,4 @@
+package com.example.StudentManagement.controller;
+
+public class RegistrationController {
+}
---
+++ src/main/java/com/example/StudentManagement/entity/RegistrationForm.java
@@ -0,0 +1,4 @@
+package com.example.StudentManagement.entity;
+
+public class RegistrationForm {
+}
