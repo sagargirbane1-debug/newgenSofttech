03bed5414374567f34a1e72cc47f890899649dc8
Sagar Girbane <your_email@example.com>
2025-09-12 13:37:40 +0530

    previous murge deleted

--- src/main/java/com/example/StudentManagement/controller/RegistrationController.java
+++
@@ -1,4 +0,0 @@
-package com.example.StudentManagement.controller;
-
-public class RegistrationController {
-}
--- src/main/java/com/example/StudentManagement/entity/RegistrationForm.java
+++
@@ -1,4 +0,0 @@
-package com.example.StudentManagement.entity;
-
-public class RegistrationForm {
-}
